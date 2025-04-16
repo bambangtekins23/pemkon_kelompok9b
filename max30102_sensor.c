45d29ed9e671f42c7f75df7aa89ecbdf62fc956e
bambangtekins23 <bangpmrt@gmail.com>
2025-04-16 15:12:02 +0700

    Create max30102_sensor.c

---
+++ max30102_sensor.c
@@ -0,0 +1,11 @@
+#include "max30102_sensor.h"
+
+uint16_t ReadHeartRate(void) {
+    // Simulasi pembacaan dari MAX30102, ganti dengan implementasi sensor asli
+    return 72;
+}
+
+uint8_t ReadSpO2(void) {
+    // Simulasi pembacaan dari MAX30102, ganti dengan implementasi sensor asli
+    return 97;
+}
