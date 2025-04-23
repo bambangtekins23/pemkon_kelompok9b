5bd98928ee741ee26101ac8514c1abd64aad7d9c
bambangtekins23 <bangpmrt@gmail.com>
2025-04-24 00:07:35 +0700

    Update and rename threshold_check.c to buzzer.c

---
+++ buzzer.c
@@ -0,0 +1,16 @@
+#include "buzzer.h"
+
+void Buzzer_Init(void)
+{
+    // Konfigurasi pin GPIO sebagai output
+}
+
+void Buzzer_On(void)
+{
+    // Set GPIO High
+}
+
+void Buzzer_Off(void)
+{
+    // Set GPIO Low
+}
